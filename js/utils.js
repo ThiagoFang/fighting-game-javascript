9aa28b30742320f5cfe8f1e35a41e5fb5e790803
Thiago <thiagofang@gmail.com>
2022-07-14 16:52:16 -0300

    refactoring more content from index.js to others files

---
+++ js/utils.js
@@ -0,0 +1,40 @@
+const rectangularCollision = ({ rectangle1, rectangle2 }) => {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    );
+};
+
+const determineWinner = ({player, enemy, timerId}) => {
+    const displayText = document.querySelector('#display-text');
+    clearTimeout(timerId)
+
+    displayText.style.display = 'flex'; 
+    if(player.health === enemy.health) {
+        displayText.innerHTML = 'Tie';
+    };
+    if(player.health > enemy.health) {
+        displayText.innerHTML = 'Player 1 Wins';
+    };
+    if(player.health < enemy.health) {
+        displayText.innerHTML = 'Player 2 Wins';
+    };
+}
+
+let timer = 60;
+let timerId;
+const decreaseTimer = () => {
+    const gameTimer = document.querySelector('#timer');
+
+    if(timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--;
+        gameTimer.innerHTML = timer;
+    };
+
+    if(timer === 0) {
+        determineWinner({player, enemy, timerId});
+    };
+};
